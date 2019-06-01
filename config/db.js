3e6eafde3bace52d147042d52aa46e55e2f9edc5
Chris <christopher.dollhopf@chasacademy.se>
2019-06-01 18:41:36 +0200

    second commit, connect to MongoDB with mongoose

---
+++ config/db.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const config = require('config'); // bring in the config file
+// then to get the mongoURI variable we place it in 
+const db = config.get('mongoURI'); // here we can get any of the values in the json file
+// if we use mongoose.connect(db) it will return a promise but then we would be required use
+// .catch .then syntax BUT in this course we are going to be using async await because it 
+// seems to be the new standard as is much cleaner and it make your code look like it is 
+// synchronous even thought it is asynchronous so async await will be used in this project.
+
+// whenever we use asnyc await we are going to wrap it in a try catch block like so:
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true // warning to use after first db connect from server.js
+    }); // await for mongoose to return a promise (useNewUrlParser: true came in later).
+    console.log('MongoDB Connected...')
+  } catch {
+    console.error(err.message) // if somthing goes wrong console an error with the err value which has a message prop on it
+    // Exit process with failure
+    process.exit(1)
+  }
+}
+// then export the module connectDB method or function
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -4,21 +4,27 @@
 
 const express = require('express');  // 1.
 
+// 7. bring in the db.js module
+const connectDB = require('./config/db')
+
 const app = express(); // 2. initialise our app variable with express
 
+// 8.  Connect database:
+connectDB();
+
 app.get('/', (req, res) => res.send('API Running'));
-// do a single endpoint just to test with app.get request to ('/') and
-// pu in our callback with (req, res) and then do a res.send('Api Running')
+// 5.  do a single endpoint just to test with app.get request to ('/') and
+// put in our callback with (req, res) and then do a res.send('Api Running')
 // which will send data to the browser saying API Running
 
 const PORT = process.env.PORT || 5000;
-// 4 put the port in a variable here which looks for environment variable
+// 4.  put the port in a variable here which looks for environment variable
 // port to use because when we deploy to Heroku thats where it is going to
 // to get the port number || but if there is no environment variable set
 // then it will default to 5000.
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-// 3 app.listen();to the port
-// 6 pass in the port to listen and then do a callback and if we want it to 
+// 3.  Take that app variable and listen to it on a port app.listen();to the port
+// 6.  pass in the port to listen and then do a callback and if we want it to 
 // do something once it connects then we console.log with backticks and throw
-// in the ${PORT} variable
+// in the ${PORT} variable.
